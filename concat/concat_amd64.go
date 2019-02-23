143c80f2a4693f3776d5fb35f3a4264d10509b69
arseny <arseny.sazanov@gmail.com>
2019-02-23 17:37:09 +0300

    fix. concat

--- concat/concat_amd64.go
+++ concat/concat_amd64.go
@@ -2,3 +2,16 @@ package concat
 
 // Strings returns x+y concatenation result.
 func Strings(x, y string) string
+
+// StringsMulti - multiply strings concat
+func StringsMulti(args ...string) string {
+	return recursiveConcat(args)
+}
+
+func recursiveConcat(ss []string) string {
+	if len(ss) > 1 {
+		return Strings(ss[0], recursiveConcat(ss[1:]))
+	} else {
+		return ss[0]
+	}
+}
