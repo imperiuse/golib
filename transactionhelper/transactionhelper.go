4801a89b8d02bc16bd896a7b1a5250e333cb7636
asazanov <asazanov@beget.ru>
2019-07-08 14:44:08 +0300

    add some new helpfully pkg
    dirHelper - for work with os filesystem
    transactionhelper - work with sqlx transaction
    server - simple universaly base for tcp/udp server
    regenerate-proto - util for help with generate grpc code from prototype files

---
+++ transactionhelper/transactionhelper.go
@@ -0,0 +1,53 @@
+package transactionhelper
+
+import (
+	"database/sql"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+)
+
+// Transaction is an interface that models the standard transaction in
+// `database/sql`.
+//
+// To ensure `TxFn` funcs cannot commit or rollback a transaction (which is
+// handled by `WithTransaction`), those methods are not included here.
+type Transaction interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	Prepare(query string) (*sql.Stmt, error)
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+}
+
+// A Txfn is a function that will be called with an initialized `Transaction` object
+// that can be used for executing statements and queries against a database.
+type TxFn func(Transaction) error
+
+// WithTransaction creates a new transaction and handles rollback/commit based on the
+// error object returned by the `TxFn`
+func WithTransaction(db *sqlx.DB, fn TxFn) (err error) {
+	var tx *sql.Tx
+	tx, err = db.Begin()
+	if err != nil {
+		return
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			// a panic occurred, rollback and repanic
+			errR := tx.Rollback()
+			err = errors.WithMessagef(err, "Panic in WithTransaction: %v. --> Rollback error: %v", p, errR)
+		} else if err != nil {
+			// something went wrong, rollback
+			errR := tx.Rollback()
+			err = errors.WithMessagef(err, "Err while execute fn. --> Rollback error: %v", errR)
+		} else {
+			// all good, commit
+			err = tx.Commit()
+		}
+	}()
+
+	err = fn(tx)
+
+	return
+}
