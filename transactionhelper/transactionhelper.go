2d3f94893d8320480a81664b6959d072d8296163
asazanov <asazanov@beget.ru>
2019-08-05 18:41:09 +0300

    transaction helper refactor, add ctx support

--- transactionhelper/transactionhelper.go
+++ transactionhelper/transactionhelper.go
@@ -1,55 +1,77 @@
 package transactionhelper
 
 import (
+	"context"
 	"database/sql"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 )
 
-// Transaction is an interface that models the standard transaction in
-// `database/sql`.
-//
-// To ensure `TxFn` funcs cannot commit or rollback a transaction (which is
-// handled by `WithTransaction`), those methods are not included here.
-type Transaction interface {
-	sqlx.QueryerContext
-	sqlx.PreparerContext
-	sqlx.ExecerContext
-	sqlx.Execer
-	sqlx.Queryer
-	sqlx.Preparer
-}
-
 // A Txfn is a function that will be called with an initialized `Transaction` object
 // that can be used for executing statements and queries against a database.
-type TxFn func(Transaction) error
+type TxFn func(*sqlx.Tx) error
 
 // WithTransaction creates a new transaction and handles rollback/commit based on the
 // error object returned by the `TxFn`
 func WithTransaction(db *sqlx.DB, fn TxFn) (err error) {
-	var tx *sql.Tx
-	tx, err = db.Begin()
-	if err != nil {
-		return
-	}
-
+	var tx *sqlx.Tx
 	defer func() {
 		if p := recover(); p != nil {
-			// a panic occurred, rollback and repanic
-			errR := tx.Rollback()
-			err = errors.WithMessagef(err, "Panic in WithTransaction: %v. --> Rollback error: %v", p, errR)
-		} else if err != nil {
-			// something went wrong, rollback
-			errR := tx.Rollback()
-			err = errors.WithMessagef(err, "Err while execute fn. --> Rollback error: %v", errR)
-		} else {
-			// all good, commit
-			err = tx.Commit()
+			err = errors.WithMessagef(err, "Panic was: %v", p)
 		}
 	}()
+	func() {
+		tx = db.MustBegin()
 
-	err = fn(tx)
+		defer func() {
+			if p := recover(); p != nil {
+				// a panic occurred, rollback and repanic
+				errR := tx.Rollback()
+				err = errors.WithMessagef(err, "Panic in WithTransaction: %v. --> Rollback error: %v", p, errR)
+			} else if err != nil {
+				// something went wrong, rollback
+				errR := tx.Rollback()
+				err = errors.WithMessagef(err, "Err while execute fn. --> Rollback error: %v", errR)
+			} else {
+				// all good, commit
+				err = tx.Commit()
+			}
+		}()
 
+		err = fn(tx)
+	}()
+	return
+}
+
+// WithCtxTransaction creates a new transaction with ctx and handles rollback/commit based on the
+// error object returned by the `TxFn`
+func WithCtxTransaction(ctx context.Context, opt *sql.TxOptions, db *sqlx.DB, fn TxFn) (err error) {
+	var tx *sqlx.Tx
+	defer func() {
+		if p := recover(); p != nil {
+			err = errors.WithMessagef(err, "Panic was: %v", p)
+		}
+	}()
+	func() {
+		tx = db.MustBeginTx(ctx, opt)
+
+		defer func() {
+			if p := recover(); p != nil {
+				// a panic occurred, rollback and repanic
+				errR := tx.Rollback()
+				err = errors.WithMessagef(err, "Panic in WithTransaction: %v. --> Rollback error: %v", p, errR)
+			} else if err != nil {
+				// something went wrong, rollback
+				errR := tx.Rollback()
+				err = errors.WithMessagef(err, "Err while execute fn. --> Rollback error: %v", errR)
+			} else {
+				// all good, commit
+				err = tx.Commit()
+			}
+		}()
+
+		err = fn(tx)
+	}()
 	return
 }
