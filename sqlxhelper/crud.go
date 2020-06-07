2577d7461fc92cfb09e9e5780e460c1dccf51ff8
Arseny Sazanov <a.sazanov@propellerads.net>
2020-06-08 01:09:15 +0300

    [SQLX_HELPER] more simple

--- sqlxhelper/crud.go
+++ sqlxhelper/crud.go
@@ -2,7 +2,6 @@ package sqlxhelper
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -11,10 +10,6 @@ import (
 // query := `INSERT INTO table (col1, col2) VALUES ($1, $2) RETURNING id`
 func InsertAndGetLastID(ctx context.Context, lastInsertID *int64, query string, args ...interface{}) TxFn {
 	return func(t *sqlx.Tx) error {
-		err := t.QueryRowContext(ctx, query, args...).Scan(lastInsertID)
-		if err != nil {
-			return fmt.Errorf("InsertAndGetLastID t.QueryRowContext err: %w  query: %s", err, query)
-		}
-		return nil
+		return t.QueryRowContext(ctx, query, args...).Scan(lastInsertID)
 	}
 }
