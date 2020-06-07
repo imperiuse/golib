cbfde64c59b102da02e0faece145d0a70d864f7f
Arseny Sazanov <a.sazanov@propellerads.net>
2020-06-07 22:21:48 +0300

    [SQLX_HELPER] Change logic of function with context

--- sqlxhelper/crud.go
+++ sqlxhelper/crud.go
@@ -1,6 +1,7 @@
 package sqlxhelper
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
@@ -8,15 +9,15 @@ import (
 
 // Usefully for sql query like this
 // query := `INSERT INTO table (col1, col2) VALUES ($1, $2) RETURNING id`
-func InsertAndGetLastID(lastInsertID *int64, query string, args ...interface{}) TxFn {
+func InsertAndGetLastID(ctx context.Context, lastInsertID *int64, query string, args ...interface{}) TxFn {
 	return func(t *sqlx.Tx) error {
-		stmt, err := t.Prepare(query)
+		stmt, err := t.PrepareContext(ctx, query)
 		if err != nil {
 			return fmt.Errorf("InsertAndGetLastID t.Prepare(query) err: %w  query: %s", err, query)
 		}
 		defer func() { _ = stmt.Close() }()
 
-		err = stmt.QueryRow(args...).Scan(lastInsertID)
+		err = stmt.QueryRowContext(ctx, args...).Scan(lastInsertID)
 		if err != nil {
 			return fmt.Errorf("InsertAndGetLastID stmt.QueryRow err: %w", err)
 		}
