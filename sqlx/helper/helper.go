e9ff8f37209f405595617df0f1768726a6351ee9
Arseny Sazanov <arseny.sazanov@gmail.com>
2021-02-27 19:23:23 +0300

    integrate golangci-lint, fix problems

--- sqlx/helper/helper.go
+++ sqlx/helper/helper.go
@@ -6,8 +6,8 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-// Usefully for sql query like this
-// query := `INSERT INTO table (col1, col2) VALUES ($1, $2) RETURNING id`
+// InsertAndGetLastID helper which Usefully for sql query like this
+// query := `INSERT INTO table (col1, col2) VALUES ($1, $2) RETURNING id`.
 func InsertAndGetLastID(ctx context.Context, lastInsertID interface{}, query string, args ...interface{}) TxFn {
 	return func(t *sqlx.Tx) error {
 		return t.QueryRowContext(ctx, query, args...).Scan(lastInsertID)
