e9f1d2a1bf9f77918cad749be3de05c83451a9b7
Arseny Sazanov <arseny.sazanov@gmail.com>
2021-04-04 20:16:44 +0300

    remove pointers from method

--- sqlx/repository/factory.go
+++ sqlx/repository/factory.go
@@ -60,7 +60,7 @@ type (
 		FindByWithInnerJoin(context.Context, []Column, Alias, Join, Condition, DTO) error
 		FindOneByWithInnerJoin(context.Context, []Column, Alias, Join, Condition, DTO) error
 
-		SelectWithPagePagination(context.Context, squirrel.SelectBuilder, *PagePaginationParams, DTO) (*PagePaginationResults, error)
+		SelectWithPagePagination(context.Context, squirrel.SelectBuilder, PagePaginationParams, DTO) (PagePaginationResults, error)
 
 		GetRowsByQuery(ctx context.Context, qb squirrel.SelectBuilder) (*sql.Rows, error)
 		CountByQuery(ctx context.Context, qb squirrel.SelectBuilder) (uint64, error)
